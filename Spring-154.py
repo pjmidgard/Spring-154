ba7e9dc762f639e47df5ef0e01197c5b6bd1652b
Java Software <pj.midgard@yahoo.com>
2022-11-04 16:22:14 +0000

    Update Spring-154.py

--- Spring-154.py
+++ Spring-154.py
@@ -198,9 +198,10 @@ class compression:
                                                 Times_10+=1
                                                 between_of_the_cirlce_of_the_file5=0
                                             if Times_10==(2**48)-1:
-                                                Times_7+=0
+                                                Times_7+=1
                                                 Times_10=0
                                                 between_of_the_cirlce_of_the_file5=1
+                                                
                                             
                                             
                                             
