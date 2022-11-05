ea09bf4219a64def771d5009b1f336439d41bc8b
Java Software <pj.midgard@yahoo.com>
2022-11-05 12:41:14 +0000

    Update Spring-154.py

--- Spring-154.py
+++ Spring-154.py
@@ -185,7 +185,7 @@ class compression:
                                     Extract1=0
                                     Times_10=0
                                     Times_7=0
-                                    between_of_the_cirlce_of_the_file5=-1
+                                    between_of_the_cirlce_of_the_file5=26
                                     
 
                                     while Extract1!=1:
@@ -200,11 +200,11 @@ class compression:
 
                                             if between_of_the_cirlce_of_the_file5==(2**16)-1:
                                                 Times_10+=1
-                                                between_of_the_cirlce_of_the_file5=0
+                                                between_of_the_cirlce_of_the_file5=26
                                             if Times_10==(2**48)-1:
                                                 Times_7+=1
                                                 Times_10=0
-                                                between_of_the_cirlce_of_the_file5=0
+                                                between_of_the_cirlce_of_the_file5=26
                                             
                                                 
                                             
