2d6f976742399a63ebaf9f500c93608050e7097f
Java Software <pj.midgard@yahoo.com>
2022-11-09 19:30:38 +0000

    Update Spring-155.py

--- Spring-155.py
+++ Spring-155.py
@@ -359,7 +359,7 @@ class compression:
                                                 
                                                 Hole_Number_information=(2**Deep5)
                                                 add_ones_together=Hole_Number_information
-                                                non_integers3=str(Number)+"."+str(non_integers)
+                                                non_integers3=str(Number)+"."+str(non_integers2)
                                                 add_ones_together=float(non_integers3)
                                                 Number_of_the_file=Number_of_the_file**add_ones_together
                                                 Number_of_the_file=int(Number_of_the_file)                                    
